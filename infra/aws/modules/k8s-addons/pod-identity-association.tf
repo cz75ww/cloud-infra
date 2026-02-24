f722b12187859543cf3eb4c5673543235a3e3a54
cz75ww <fabiano.prospero@gmail.com>
2026-02-24 16:49:36 +0100

    Added PIA Association and rename secrets reference

--- infra/aws/modules/k8s-addons/pod-identity-association.tf
+++ infra/aws/modules/k8s-addons/pod-identity-association.tf
@@ -1,3 +1,6 @@
+##############################################
+# ebs_csi Pod Identity Association
+##############################################
 resource "aws_eks_pod_identity_association" "ebs_csi" {
   cluster_name    = var.eks_name
   namespace       = "kube-system"
@@ -5,9 +8,22 @@ resource "aws_eks_pod_identity_association" "ebs_csi" {
   role_arn        = aws_iam_role.ebs_csi.arn
 }
 
-resource "aws_eks_pod_identity_association" "secrets_store_test" {
+##############################################
+# SecretStore Pod Identity Association
+##############################################
+resource "aws_eks_pod_identity_association" "secrets_store" {
   cluster_name    = var.eks_name
   namespace       = "default"
   service_account = "default"
-  role_arn        = aws_iam_role.secrets_store_test.arn
+  role_arn        = aws_iam_role.secrets_store.arn
+}
+
+##############################################
+# ExternalDNS Pod Identity Association
+##############################################
+resource "aws_eks_pod_identity_association" "external_dns" {
+  cluster_name    = var.eks_name
+  namespace       = "external-dns"
+  service_account = "external-dns"
+  role_arn        = aws_iam_role.externaldns_role.arn
 }
