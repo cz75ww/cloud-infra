c02024571acfe62db32bb73ead8f1ebeed57e559
cz75ww <fabiano.prospero@gmail.com>
2026-02-24 16:54:21 +0100

    removed spaces

---
+++ infra/aws/modules/k8s-addons/data.tf
@@ -0,0 +1,19 @@
+# Get OIDC provider (needed for IRSA)
+data "aws_eks_cluster" "this" {
+  name = var.eks_name  
+}
+
+data "aws_iam_openid_connect_provider" "eks" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole", "sts:TagSession"]
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+  }
+}
