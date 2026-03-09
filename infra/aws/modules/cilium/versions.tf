9624ca09e58e7f4ef33cfa5371f31b0e2521e677
cz75ww <fabiano.prospero@gmail.com>
2026-03-09 19:09:04 +0100

    New providers required

---
+++ infra/aws/modules/cilium/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
+  }
+}
