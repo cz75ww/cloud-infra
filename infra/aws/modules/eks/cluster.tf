e0cf9a2116e7d5c52aff8e79a1c412b64e199a22
cz75ww <fabiano.prospero@gmail.com>
2026-04-01 12:22:55 +0200

    Fixed the cluster name variable

--- infra/aws/modules/eks/cluster.tf
+++ infra/aws/modules/eks/cluster.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role_policy_attachment" "eks" {
 }
 
 resource "aws_eks_cluster" "this" {
-  name     = "${var.env}-${var.eks_name}"
+  name     = var.eks_name
   version  = var.eks_version
   role_arn = aws_iam_role.eks.arn
   bootstrap_self_managed_addons = false
